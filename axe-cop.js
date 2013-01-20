4d262c2d605e44bd51c9f9160acfa57790506381
Adam Pearson <adam@substantial.com>
2013-01-19 19:55:12 -0800

    Can jump to a page by pressing 'g'.

--- axe-cop.js
+++ axe-cop.js
@@ -52,4 +52,15 @@ $(function(){
 	index -= 1;
 	showCurrent();
     });
+
+    jwerty.key('g', function(){
+	$('#options').toggleClass('show');
+    });
+
+    $('#options').submit(function(e){
+	e.preventDefault();
+	var input = $(this).find('input[name="pageNum"]');
+	index = parseInt(input.val(), 10);
+	showCurrent();
+    });
 });
